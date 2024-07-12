bf5383eb22bf6dd4b656eb09fce51e9df2bc968a
21-manic-006g <wayz1@naver.com>
2024-07-12 09:45:59 +0900

    0712 intellij

---
+++ src/g150/Main.java
@@ -0,0 +1,17 @@
+package g150;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arg){
+
+
+    Scanner sc = new Scanner(System.in);
+    String[] arr = new String[10];
+
+    for(int i = 0; i < 10; i++){
+        String a = sc.next();
+
+    }
+}
+}
